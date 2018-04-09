6411a9887f7a5112cf49e263dd7676abb852dfff
Andrew Crawford <andrew.crawford.za@gmail.com>
2018-04-09 20:05:40 +0200

    MOre functions added

--- main.js
+++ main.js
@@ -3,7 +3,9 @@
 export const a = 'the file text a';
 export const b = 'the file text b';
 export const q = 'the file text q';
-
+export const q = 'the file text q';
+export const q = 'the file text q';
+export const q = 'the file text q';
 
 // more text
 
